558d15c4a8933562eedb2aaffa2a7eb6e906bc38
wangyang <yang1.wang@baidao.com>
2017-08-02 15:26:44 +0800

    翼手龙monster

---
+++ Assets/Scripts/FlyDragon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyDragon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Reset() {
+		int level = Random.Range (1, 4);
+		Vector3 oldPos = this.transform.position;
+		oldPos.y = level;
+		this.transform.position = oldPos;
+	}
+}
--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -6,10 +6,12 @@ public class MonsterController : MonoBehaviour {
 
 	public GameObject[] monsterTemplates;
 	private const int MonsterPoolSize = 30;
-	private ArrayList activeMonsterInstances;
-	private ArrayList inactiveMonsterInstances;
+	private ArrayList[] activeMonsterInstances;
+	private ArrayList[] inactiveMonsterInstances;
 	// Use this for initialization
 	void Start () {
+		activeMonsterInstances = new ArrayList[monsterTemplates.Length];
+		inactiveMonsterInstances = new ArrayList[monsterTemplates.Length];
 		CreateMonsterPool ();
 	}
 
@@ -21,14 +23,14 @@ public class MonsterController : MonoBehaviour {
 	void NewGroundDidCreate(GameObject ground) {
 		Renderer groundRenderer = ground.GetComponent<Renderer> ();
 		float width = groundRenderer.bounds.size.x;
-		int count = Random.Range (1, 4);
-		float[] genPositions = new float[count];
+		int monsterIndex = Random.Range (0, monsterTemplates.Length);
+		float[] genPositions = new float[1];
 		float value = Random.Range ( width / 3, width * 2 / 3);
-		for (int i = 0; i < count; ++i) {
+		for (int i = 0; i < 1; ++i) {
 			genPositions [i] = value + i * 1.2f;
 		}
 		for (int i = 0; i < genPositions.Length; ++i) {
-			GameObject monster = GetMonsterFromPool ();
+			GameObject monster = GetMonsterFromPool (monsterIndex);
 			if (monster != null) {
 				Vector3 oldPos = monster.transform.position;
 				monster.transform.position = new Vector3(ground.transform.position.x + genPositions[i], oldPos.y ,ground.transform.position.z);
@@ -37,48 +39,55 @@ public class MonsterController : MonoBehaviour {
 	}
 
 	void CreateMonsterPool() {
-		activeMonsterInstances = new ArrayList ();
-		inactiveMonsterInstances = new ArrayList ();
-		for (int i = 0; i < MonsterPoolSize; ++i) {
-			GameObject monster = GameObject.Instantiate (monsterTemplates [0]);
-			monster.SetActive (false);
-			inactiveMonsterInstances.Add (monster);
+		for (int monsterIndex = 0; monsterIndex < monsterTemplates.Length; ++monsterIndex) {
+			activeMonsterInstances[monsterIndex] = new ArrayList ();
+			inactiveMonsterInstances[monsterIndex] = new ArrayList ();
+			for (int i = 0; i < MonsterPoolSize; ++i) {
+				GameObject monster = GameObject.Instantiate (monsterTemplates [monsterIndex]);
+				monster.SetActive (false);
+				inactiveMonsterInstances[monsterIndex].Add (monster);
+			}
+			monsterTemplates [monsterIndex].SetActive (false);
 		}
-		monsterTemplates [0].SetActive (false);
 	}
 
 	void CollectInactiveMonster() {
-		for (int i = 0; i < activeMonsterInstances.Count; ++i) {
-			GameObject monster = activeMonsterInstances [i] as GameObject;
-			Renderer monsterRenderer = monster.GetComponent<Renderer> ();
-			if (Camera.main.WorldToScreenPoint(monsterRenderer.bounds.center + monsterRenderer.bounds.size / 2).x < 0.0) {
-				Debug.Log ("Monster Out");
-				inactiveMonsterInstances.Add (monster);
-				monster.SetActive (false);
-				activeMonsterInstances.RemoveAt (i);
-				i--;
+		for (int monsterIndex = 0; monsterIndex < monsterTemplates.Length; ++monsterIndex) {
+			for (int i = 0; i < activeMonsterInstances[monsterIndex].Count; ++i) {
+				GameObject monster = activeMonsterInstances[monsterIndex] [i] as GameObject;
+				Renderer monsterRenderer = monster.GetComponent<Renderer> ();
+				if (Camera.main.WorldToScreenPoint (monsterRenderer.bounds.center + monsterRenderer.bounds.size / 2).x < 0.0) {
+					Debug.Log ("Monster Out");
+					inactiveMonsterInstances[monsterIndex].Add (monster);
+					monster.SetActive (false);
+					activeMonsterInstances[monsterIndex].RemoveAt (i);
+					i--;
+				}
 			}
 		}
 	}
 
-	GameObject GetMonsterFromPool() {
-		if (inactiveMonsterInstances.Count > 0) {
-			GameObject monster = inactiveMonsterInstances [0] as GameObject;
-			inactiveMonsterInstances.RemoveAt (0);
-			activeMonsterInstances.Add (monster);
+	GameObject GetMonsterFromPool(int monsterIndex) {
+		if (inactiveMonsterInstances[monsterIndex].Count > 0) {
+			GameObject monster = inactiveMonsterInstances[monsterIndex][0] as GameObject;
+			inactiveMonsterInstances[monsterIndex].RemoveAt (0);
+			activeMonsterInstances[monsterIndex].Add (monster);
 			monster.SetActive (true);
+			monster.SendMessage ("Reset");
 			return monster;
 		}
 		return null;
 	}
 
 	public void Reset() {
-		for (int i = 0; i < activeMonsterInstances.Count; ++i) {
-			GameObject monster = activeMonsterInstances [i] as GameObject;
-			monster.SetActive (false);
+		for (int monsterIndex = 0; monsterIndex < monsterTemplates.Length; ++monsterIndex) {
+			for (int i = 0; i < activeMonsterInstances[monsterIndex].Count; ++i) {
+				GameObject monster = activeMonsterInstances[monsterIndex] [i] as GameObject;
+				monster.SetActive (false);
+			}
+			inactiveMonsterInstances[monsterIndex].AddRange (activeMonsterInstances[monsterIndex]);
+			activeMonsterInstances[monsterIndex].RemoveRange (0, activeMonsterInstances[monsterIndex].Count);
 		}
-		inactiveMonsterInstances.AddRange (activeMonsterInstances);
-		activeMonsterInstances.RemoveRange (0, activeMonsterInstances.Count);
 
 	}
 }
